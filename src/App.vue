b24de15c793b615fc906aaac7eed428a67282a39
ウスマーン <82204694+uthmaanb@users.noreply.github.com>
2022-11-02 19:08:58 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<h1>{{ title }}</h1>
+	<p>welcome...</p>
+	<teleport to=".modals" v-if="showModal">
+		<Modal theme="sale" @close="toggleModal">
+			<template v-slot:links>
+				<a href="#">sign up now</a>
+				<a href="#">more info</a>
+			</template>
+			<h1>Ninja Giveaway!</h1>
+			<p>grab your ninja swag for half price!</p>
+		</Modal>
+	</teleport>
+	<div v-if="showModal2">
+		<Modal @close="toggleModal2">
+			<template v-slot:links>
+				<a href="#">sign up now</a>
+				<a href="#">more info</a>
+			</template>
+			<h1>Ninja Giveaway!</h1>
+			<p>grab your ninja swag for half price! this is number 2</p>
+		</Modal>
+	</div>
+	<button @click.alt="toggleModal">open modal (alt)</button>
+	<button @click.alt="toggleModal2">open modal2 (alt)</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+	import Modal from "./components/Modal";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		name: "App",
+		components: { Modal },
+		data() {
+			return {
+				title: "Vue App",
+				header: "Signup for the giveaway!",
+				text: "Grab your ninja swag",
+				showModal: false,
+				showModal2: false,
+			};
+		},
+
+		methods: {
+			toggleModal() {
+				this.showModal = !this.showModal;
+			},
+			toggleModal2() {
+				this.showModal2 = !this.showModal2;
+			},
+		},
+	};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app, .modals {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+	h1 {
+		border-bottom: 1px solid #ddd;
+		display: inline-block;
+		padding-bottom: 10px;
+	}
 </style>
